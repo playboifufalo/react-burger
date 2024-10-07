2b6079caf0c0c66fa5e39847e98f971b7ff1f70a
playboifufalo <krasvorp@gmail.com>
2024-10-07 23:18:00 +0300

    //

--- src/services/actions/constructor-action.js
+++ src/services/actions/constructor-action.js
@@ -1,3 +1,4 @@
+import checkResponse from '../../utils/check-response';
 import { BASE_URL } from '../../utils/const';
 import { v4 as uuidv4 } from 'uuid';
 import {
@@ -52,7 +53,7 @@ export const fetchIngredients = () => async (dispatch) => {
 
   try {
     const response = await fetch(`${BASE_URL}ingredients`);
-    const data = await response.json();
+    const data = await checkResponse(response);
     dispatch({ type: FETCH_INGREDIENTS_SUCCESS, payload: data.data });
   } catch (error) {
     dispatch({ type: FETCH_INGREDIENTS_FAILURE, payload: 'Failed to load ingredients' });
